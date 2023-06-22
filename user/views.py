bce01ead04b4345efdbaec1c3ba7fc03c1b9a1e9
selim-haspolat <118964736+selim-haspolat@users.noreply.github.com>
2023-06-22 22:58:18 +0300

    user

--- user/views.py
+++ user/views.py
@@ -1,6 +1,8 @@
+REDIRECT_URL_PATHNAME = 'user_login'
+
 class FixView:
     template_name = 'user/user_form.html'
-    next_page = 'user_login'
+    next_page = REDIRECT_URL_PATHNAME
 
 
 # ----------------------------
@@ -22,6 +24,17 @@ class UserLogoutView(FixView, LogoutView):
 # ----------------------------
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
+# from .forms import CustomUserCreationForm
+from django.urls import reverse_lazy
+
 
 class UserCreateView(FixView, CreateView):
     form_class = UserCreationForm
+    # success_url = '/user/login/'
+    success_url = reverse_lazy(REDIRECT_URL_PATHNAME)
+
+    # Login after registration:
+    def get_success_url(self):
+        from django.contrib.auth import login
+        login(self.request, self.object)
+        return super().get_success_url()
